dffa4d4b92cf983b531576a9fdefe85b62e9500d
13fragment <mgolubevbiz@gmail.com>
2023-05-18 09:18:15 +0300

    git init

---
+++ src/main/java/abc/investbot/InvestBotApplication.java
@@ -0,0 +1,13 @@
+package abc.investbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/abc/investbot/InvestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package abc.investbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
