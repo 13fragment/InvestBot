14d6c92b1213810a4388c62f3fe27a8ec821e16a
13fragment <mgolubevbiz@gmail.com>
2023-06-03 20:10:51 +0300

    change config: - migrate clientDTO files to the service - create AccountService java class

--- src/main/java/abc/investbot/service/AccountService.java
+++ src/main/java/abc/investbot/service/AccountService.java
@@ -1,13 +1,13 @@
-package abc.investbot.clientDTO;
+package abc.investbot.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import ru.tinkoff.piapi.core.InvestApi;
 @Configuration
-public class Account {
+public class AccountService {
     private InvestApi investApi;
- /** Create setter for sandboxAccount and getter for investApi */
-    public Account(@Value("${app.config.token}") String token,@Value("${app.config.sandbox-mode}") boolean sandboxMode)
+    /** Create setter for sandboxAccount and getter for investApi */
+    public AccountService(@Value("${app.config.token}") String token,@Value("${app.config.sandbox-mode}") boolean sandboxMode)
     {
         if (token == null || token.isBlank()) {
             throw new IllegalArgumentException("token is not defined");
