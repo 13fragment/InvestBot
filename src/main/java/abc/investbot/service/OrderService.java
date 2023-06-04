98b37676dde31fe065d76710793fbcdab0e0267a
13fragment <mgolubevbiz@gmail.com>
2023-06-05 00:28:16 +0300

    create template for OrderService

--- src/main/java/abc/investbot/service/OrderService.java
+++ src/main/java/abc/investbot/service/OrderService.java
@@ -1,4 +1,14 @@
 package abc.investbot.service;
+import org.springframework.stereotype.Service;
 
-public class OrderService {
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class OrderService{
+    private final Map<String, String> figi = new HashMap<String, String>();
+    private final AccountService accountService;
+    public OrderService(AccountService accountService) {
+        this.accountService = accountService;
+    }
 }
