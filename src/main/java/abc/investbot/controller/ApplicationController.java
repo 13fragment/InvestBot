2f30f98265244dcf705eb2e34aaf83b699a08091
13fragment <mgolubevbiz@gmail.com>
2023-06-04 17:19:44 +0300

    create first get mapping request

--- src/main/java/abc/investbot/controller/ApplicationController.java
+++ src/main/java/abc/investbot/controller/ApplicationController.java
@@ -1,7 +1,21 @@
 package abc.investbot.controller;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.bind.annotation.GetMapping;
-//@RestController
-public class ApplicationController {
+import abc.investbot.service.AccountService;
+import abc.investbot.service.SandboxService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@RestController ()
+public class ApplicationController {
+    private final SandboxService sandboxService;
+    private static final Logger log = LoggerFactory.getLogger(SandboxService.class);
+
+    public ApplicationController(AccountService accountService, SandboxService sandboxService) {
+        this.sandboxService = sandboxService;
+    }
+    @GetMapping("/portfolio")
+    public String portfolioRequest(){
+        return String.valueOf(sandboxService.getPortfolio());
+    }
 }
--- src/main/java/abc/investbot/service/SandboxService.java
+++ src/main/java/abc/investbot/service/SandboxService.java
@@ -1,5 +1,19 @@
 package abc.investbot.service;
 
-public class SandboxService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
+@Service
+public class SandboxService {
+    private static final Logger log = LoggerFactory.getLogger(SandboxService.class);
+    private final AccountService accountService;
+
+    public SandboxService(AccountService accountService) {
+        this.accountService = accountService;
+    }
+    public String getPortfolio(){
+        log.info("Сведения о портфолио: ");
+        return String.valueOf(accountService.getInvestApi().getSandboxService().getPortfolioSync(accountService.getAccountId()));
+    }
 }
