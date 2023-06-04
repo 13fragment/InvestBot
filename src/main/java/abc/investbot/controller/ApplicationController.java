76a3b5646d7ae6f3dc0c5ede32ef036422a1cc9f
13fragment <mgolubevbiz@gmail.com>
2023-06-04 20:20:34 +0300

    modified controller structure

--- src/main/java/abc/investbot/controller/ApplicationController.java
+++ src/main/java/abc/investbot/controller/ApplicationController.java
@@ -1,17 +1,15 @@
 package abc.investbot.controller;
-import abc.investbot.service.AccountService;
 import abc.investbot.service.SandboxService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController ()
+@RequestMapping("/sandbox")
 public class ApplicationController {
     private final SandboxService sandboxService;
-    private static final Logger log = LoggerFactory.getLogger(SandboxService.class);
 
-    public ApplicationController(AccountService accountService, SandboxService sandboxService) {
+    public ApplicationController(SandboxService sandboxService) {
         this.sandboxService = sandboxService;
     }
     @GetMapping("/portfolio")
