b2a7cccc8121ecbcfdc0f6c8157759426b10f8d7
Safonova Sofia <sofia.safonova.aleksandrovna@gmail.com>
2023-06-08 15:00:35 +0300

    add OrderService

--- src/main/java/abc/investbot/controller/SandboxController.java
+++ src/main/java/abc/investbot/controller/SandboxController.java
@@ -1,32 +1,50 @@
 package abc.investbot.controller;
+
 import abc.investbot.service.SandboxService;
+import abc.investbot.service.OrderService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController ()
+
+@RestController()
 @RequestMapping("/sandbox")
 public class SandboxController {
     private final SandboxService sandboxService;
-    public SandboxController(SandboxService sandboxService) {
+    private final OrderService orderService;
+
+    @Value("${app.trading.quantity}")
+    private long quantity;
+
+    @Value("${app.trading.figi.SBER}")
+    private String figi;
+
+    public SandboxController(SandboxService sandboxService, OrderService orderService) {
         this.sandboxService = sandboxService;
+        this.orderService = orderService;
+        orderService.buyMarket("BBG004730N88", 1);
     }
-    @GetMapping(value ="/portfolio")
-    public String portfolioRequest(){
+
+    @GetMapping(value = "/portfolio")
+    public String portfolioRequest() {
         return String.valueOf(sandboxService.getPortfolio());
     }
+
     @GetMapping(value = "/receipt/{sum}")
     public String receipt(@PathVariable long sum) {
         sandboxService.addMoney(sum);
-        return "Баланс был пополнен на "+sum+" RUB";
+        return "Баланс был пополнен на " + sum + " RUB";
     }
-    @GetMapping(value ="/orders")
-    public String ordersRequest(){
+
+    @GetMapping(value = "/orders")
+    public String ordersRequest() {
         return String.valueOf(sandboxService.orders());
     }
-    @GetMapping(value ="/orders/{orderId}")
-    public String orderStateRequest(@PathVariable String orderId){
+
+    @GetMapping(value = "/orders/{orderId}")
+    public String orderStateRequest(@PathVariable String orderId) {
         return String.valueOf(sandboxService.orderState(orderId));
     }
 }
--- src/main/java/abc/investbot/service/OrderService.java
+++ src/main/java/abc/investbot/service/OrderService.java
@@ -1,14 +1,28 @@
 package abc.investbot.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import ru.tinkoff.piapi.contract.v1.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Service
-public class OrderService{
+public class OrderService {
+    private static final Logger log = LoggerFactory.getLogger(SandboxService.class);
     private final Map<String, String> figi = new HashMap<String, String>();
     private final AccountService accountService;
+
     public OrderService(AccountService accountService) {
         this.accountService = accountService;
+    }
+
+    public void buyMarket(String figi, long quantity) {
+        var accountId = accountService.getAccountId();
+        var orderId = accountService.getInvestApi().getSandboxService().postOrderSync(figi, quantity, Quotation.getDefaultInstance(), OrderDirection.ORDER_DIRECTION_BUY, accountId, OrderType.ORDER_TYPE_MARKET,
+                UUID.randomUUID().toString()).getOrderId();
+        log.info("выставлена заявка с id: {}", orderId);
     }
 }
